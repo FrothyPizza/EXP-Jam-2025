a471319ad9421ace67f2ca776a259a2f06a1a745
Hal <74992877+FrothyPizza@users.noreply.github.com>
2025-04-16 19:34:04 -0500

    add dangling spider for testing

--- js/enemies/miscEnemies.js
+++ js/enemies/miscEnemies.js
@@ -286,6 +286,77 @@ class OrbEnemy extends Enemy {
 
 
 
+// the dangling spider danges from the ceiling and moves up and down
+class DanglingSpiderEnemy extends Enemy {
+    constructor(x, y) {
+        super(x, y, 8, 8, 100); // Adjust health as needed
+        
+        this.spawnPosition = {x: x, y: y};
+        
+        this.dangleRange = 16;
+        this.dangleSpeed = 0.25;
+        this.dangleDirection = 1;
+
+        this.direction = 1;
+        this.collidesWithMap = false;
+        this.gravity = 0;
+
+        // Animation setup
+        this.animationSpeed = 6;
+        this.sprite = new AnimatedSprite(Loader.spriteSheets.spirit_walker, "Idle", this.animationSpeed);
+
+    }
+
+    findDistToCeiling(map) {
+        let dist = 0;
+        for(let i = 0; i < 100; i++) {
+            if(map.pointIsCollidingWithWall(this.x + 4, this.y - i)) {
+                dist = i;
+                break;
+            }
+        }
+        return dist;
+    }
+
+    update(map, entities) {
+        super.update(map, entities);
+        
+        if (this.dead) return;
+
+        // Handle movement
+        this.updateDangleBehavior(map);
+    }
+
+    draw(context, map) {
+        super.draw(context);
+        this.sprite.draw(context, this.x, this.y);
+
+        if(this.dead) return;
+        // draw a line to the ceiling; account for view.x (add) and view.y (subtract)
+        // fill white
+        context.strokeStyle = "white";
+        context.beginPath();
+        context.moveTo(this.x + 4 - context.view.x, Math.round(this.y + context.view.y));
+        context.lineTo(this.x + 4 - context.view.x, Math.round(this.y - Math.round(this.findDistToCeiling(map)) + context.view.y));
+        context.stroke();
+
+    }
+
+    updateDangleBehavior(map) {
+        if(this.y > this.spawnPosition.y + this.dangleRange) {
+            this.dangleDirection = -1;
+        } else if(this.y < this.spawnPosition.y) {
+            this.dangleDirection = 1;
+        }
+
+        this.y += this.dangleSpeed * this.dangleDirection;
+    }
+
+
+}
+
+
+
 
 
 
@@ -374,127 +445,4 @@ class OrbEnemy extends Enemy {
 // function findPlayer(entities) {
 //     return entities.find(entity => entity instanceof Player) || null;
 // }
-
-
-// the dangling spider danges from the ceiling and moves up and down
-class DanglingSpiderEnemy extends Enemy {
-    constructor(x, y) {
-        super(x, y, 8, 8, 100); // Adjust health as needed
-        
-        this.spawnPosition = {x: x, y: y};
-        
-        this.dangleRange = 16;
-        this.dangleSpeed = 0.25;
-        this.dangleDirection = 1;
-
-        this.direction = 1;
-        this.collidesWithMap = false;
-        this.gravity = 0;
-
-        // Animation setup
-        this.animationSpeed = 6;
-        this.sprite = new AnimatedSprite(Loader.spriteSheets.spider, "Dangle", this.animationSpeed);
-
-    }
-
-    findDistToCeiling(map) {
-        let dist = 0;
-        for(let i = 0; i < 100; i++) {
-            if(map.pointIsCollidingWithWall(this.x + 4, this.y - i)) {
-                dist = i;
-                break;
-            }
-        }
-        return dist;
-    }
-
-    update(map, entities) {
-        super.update(map, entities);
-        
-        if (this.dead) return;
-
-        // Handle movement
-        this.updateDangleBehavior(map);
-    }
-
-    draw(context, map) {
-        super.draw(context);
-        this.sprite.draw(context, this.x, this.y);
-
-        if(this.dead) return;
-        // draw a line to the ceiling; account for view.x (add) and view.y (subtract)
-        // fill white
-        context.strokeStyle = "white";
-        context.beginPath();
-        context.moveTo(this.x + 4 - context.view.x, Math.round(this.y + context.view.y));
-        context.lineTo(this.x + 4 - context.view.x, Math.round(this.y - Math.round(this.findDistToCeiling(map)) + context.view.y));
-        context.stroke();
-
-    }
-
-    updateDangleBehavior(map) {
-        if(this.y > this.spawnPosition.y + this.dangleRange) {
-            this.dangleDirection = -1;
-        } else if(this.y < this.spawnPosition.y) {
-            this.dangleDirection = 1;
-        }
-
-        this.y += this.dangleSpeed * this.dangleDirection;
-    }
-
-
-}
-
-
-
-class Run2WallEnemy extends Enemy {
-    constructor(x, y) {
-        super(x - 240, y, 240, 176, 1000000);
-        
-        this.sprite = new AnimatedSprite(Loader.spriteSheets.level_run_2_wall, "Idle", 10);
-        this.collidesWithMap = false;
-        this.gravity = 0;
-        this.defaultGravity = 0;
-
-        this.hurtboxes = [{ x: 0, y: 0, w: this.width - 6, h: this.height }];
-
-        Loader.setCurrentMusicSpeed(1);
-    }
-
-    update(map, entities) {
-        super.update(map, entities);
-
-        this.velocity.x = 0.5;
-
-
-        // find map BossCues WallSpeedUp
-        map.bossCues.forEach(cue => {
-            if(cue.name === "WallSpeedUp" && this.x + this.width > cue.x) {
-                this.velocity.x = 1;
-
-                Loader.setCurrentMusicSpeed(1.5);
-            }
-        });
-
-
-        currentScene.player.weapon = null;
-        currentScene.player.isAllowedWeapon = false;
-    }
-
-    draw(context) {
-        super.draw(context);
-
-        this.sprite.draw(context, this.x, this.y);
-    }
-
-
-    interactWith(other) {
-        if(other instanceof Player && other.colliding(this)) {
-            other.x += this.velocity.x;
-        }
-    }
-
-} 
-
-
 
